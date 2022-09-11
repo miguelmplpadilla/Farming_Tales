8dfb54906e0af203bc0befe1ef44bc79d657b838
miguelmplpadilla <miguelpadillal@hotmail.es>
2022-09-11 10:36:45 +0200

    Generador de mazmorras aleatorias

--- FarmingTales/Assets/Scripts/Escenario/MazmorraAleatoria/GeneradorMazmorra.cs
+++ FarmingTales/Assets/Scripts/Escenario/MazmorraAleatoria/GeneradorMazmorra.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Random = System.Random;
 
 public class GeneradorMazmorra : MonoBehaviour
@@ -9,6 +12,9 @@ public class GeneradorMazmorra : MonoBehaviour
     public GameObject[] salasDisponibles;
     public List<GameObject> salas;
 
+    public GameObject girar1;
+    public GameObject girar2;
+
     private GameObject grid;
     
     void Start()
@@ -16,10 +22,14 @@ public class GeneradorMazmorra : MonoBehaviour
         grid = GameObject.Find("Grid");
         
         Random random = new Random();
-        int numSalaGenerar = random.Next(5, 15);
+        int numSalaGenerar = random.Next(5, 20);
 
         int numSalaAnterior = -1;
         int numSala = 0;
+
+        Vector3 localScale = new Vector3(1, 1, 1);
+
+        GameObject salaAnterior = null;
         
         for (int i = 0; i < numSalaGenerar; i++)
         {
@@ -38,19 +48,88 @@ public class GeneradorMazmorra : MonoBehaviour
 
             GameObject salaInstanciada = Instantiate(salasDisponibles[numSala], grid.transform.position, Quaternion.identity, grid.transform);
             
+            salaInstanciada.transform.localScale = localScale;
+            
             if (i == 0)
             {
                 salaInstanciada.transform.position = new Vector3(0, 0, 0);
             }
             else
             {
-                GameObject salaAnterior = salas[i - 1].transform.GetChild(0).GetChild(0).gameObject;
+                salaAnterior = salas[i - 1].transform.GetChild(0).GetChild(0).gameObject;
                 salaInstanciada.transform.position = new Vector3(salaAnterior.transform.position.x,
                     salaAnterior.transform.position.y, salaAnterior.transform.position.z);
             }
-            
+
             salas.Add(salaInstanciada);
             
+            random = new Random();
+            int numGirar = random.Next(0, 10);
+
+            if (numGirar == 8 && ((i+1)+2) < numSalaGenerar)
+            {
+                if (localScale.x == 1)
+                {
+                    salaInstanciada = Instantiate(girar1, grid.transform.position, Quaternion.identity, grid.transform);
+                    
+                    salaAnterior = salas[i].transform.GetChild(0).GetChild(0).gameObject;
+                    salaInstanciada.transform.position = new Vector3(salaAnterior.transform.position.x,
+                        salaAnterior.transform.position.y, salaAnterior.transform.position.z);
+                    
+                    salas.Add(salaInstanciada);
+                    
+                    salaAnterior = salaInstanciada.transform.GetChild(0).GetChild(0).gameObject;
+                    
+                    salaInstanciada = Instantiate(girar2, grid.transform.position, Quaternion.identity, grid.transform);
+                    
+                    salaInstanciada.transform.position = new Vector3(salaAnterior.transform.position.x,
+                        salaAnterior.transform.position.y, salaAnterior.transform.position.z);
+                    
+                    salas.Add(salaInstanciada);
+                    
+                    localScale = new Vector3(-1, 1, 1);
+
+                    i += 2;
+                }
+                else
+                {
+                    
+                    salaAnterior = salaInstanciada.transform.GetChild(0).GetChild(0).gameObject;
+                    
+                    salaInstanciada = Instantiate(girar1, grid.transform.position, Quaternion.identity, grid.transform);
+
+                    salaInstanciada.transform.localScale = new Vector3(-1, 1, 1);
+                    
+                    salaInstanciada.transform.position = new Vector3(salaAnterior.transform.position.x,
+                        salaAnterior.transform.position.y, salaAnterior.transform.position.z);
+                    
+                    salas.Add(salaInstanciada);
+                    
+                    salaAnterior = salaInstanciada.transform.GetChild(0).GetChild(0).gameObject;
+                    
+                    salaInstanciada = Instantiate(girar2, grid.transform.position, Quaternion.identity, grid.transform);
+                    
+                    salaInstanciada.transform.localScale = new Vector3(-1, 1, 1);
+                    
+                    salaInstanciada.transform.position = new Vector3(salaAnterior.transform.position.x,
+                        salaAnterior.transform.position.y, salaAnterior.transform.position.z);
+                    
+                    salas.Add(salaInstanciada);
+                    
+                    localScale = new Vector3(1, 1, 1);
+                    
+                    i += 2;
+                }
+            }
+            
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
